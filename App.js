2abf8dc907cba290b8092a213691f82683b4ed6d
Raunak Poddar <2021mt12277@wilp.bits-pilani.ac.in>
2022-11-06 17:52:28 +0530

    Add routes for vaxed and unvaxed students

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import Login from './pages/login.js'
 import Dashboard from './pages/dashboard.js'
 import ManageStudents from './pages/manage-students.js'
 import StudentList from './pages/student-list.js'
+import VaxedStudentList from './pages/vaxed-student-list.js'
+import UnVaxedStudentList from './pages/unvaxed-student-list.js'
 
 console.reportErrorsAsExceptions = false;
 
@@ -23,7 +25,14 @@ ManageStudents: {
 },
 StudentList: {
   screen: StudentList
+},
+VaxedStudentList: {
+  screen: VaxedStudentList
+},
+UnvaxedStudentList: {
+  screen: UnVaxedStudentList
 }
+
 });
 
 const App = createAppContainer(RootStack);
